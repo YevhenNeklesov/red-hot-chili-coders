be8df3f910e8277840713faecd3fa6aed58c0a50
YevhenNeklesov <161756863+YevhenNeklesov@users.noreply.github.com>
2024-08-17 23:03:18 +0200

    fix faq.css

--- src/js/faq.js
+++ src/js/faq.js
@@ -3,7 +3,7 @@ import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 
 
-const accordionFaq = new Accordion('.accordion-container', {
+new Accordion('.accordion-container', {
     duration: 400,
     showMultiple: true,
 });
