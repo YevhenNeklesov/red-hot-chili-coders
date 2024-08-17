b2e849c718768d3e5c243358a589f38c8376f644
Tetiana Posmashna <tetiana.posmashna@gmail.com>
2024-08-17 15:07:34 +0300

    added accordion library js and css styles

---
+++ src/js/faq.js
@@ -0,0 +1,16 @@
+import Accordion from 'accordion-js';
+import 'accordion-js/dist/accordion.min.css';
+
+const accordionFaq = new Accordion('.accordion-container', {
+    duration: 400,
+    showMultiple: true,
+});
+
+const faqBtns = document.querySelectorAll('.faq-item-btn');
+
+faqBtns.forEach(button => {
+    button.addEventListener('click', () => {
+        const arrowIcon = button.querySelector('.faq-btn-icon');
+        arrowIcon.classList.toggle('arrow-up');
+    })
+});
