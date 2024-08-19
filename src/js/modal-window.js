2c61febefa0b61f51ea02af081722c3e11406c90
YevhenNeklesov <161756863+YevhenNeklesov@users.noreply.github.com>
2024-08-19 15:23:23 +0200

    complate modal window

---
+++ src/js/modal-window.js
@@ -0,0 +1,31 @@
+
+
+(() => {
+    const mobileMenu = document.querySelector('.js-modal-window');
+    const openMenuBtn = document.querySelector('.js-open-btn');
+    const closeMenuBtn = document.querySelector('.js-modal-close-btn');
+    const closeMenuLink = document.querySelectorAll('.js-modal-link');
+  
+    const toggleMenu = () => {
+        const isMenuOpen =
+            openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+        openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+        mobileMenu.classList.toggle('is-open');
+  
+        const scrollLockMethod = !isMenuOpen
+            ? 'disableBodyScroll'
+            : 'enableBodyScroll';
+        bodyScrollLock[scrollLockMethod](document.body);
+    };
+    closeMenuLink.forEach(item => item.addEventListener('click', toggleMenu));
+    openMenuBtn.addEventListener('click', toggleMenu);
+    closeMenuBtn.addEventListener('click', toggleMenu);
+//   close menu on tab
+    window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+        if (!e.matches) return;
+        mobileMenu.classList.remove('is-open');
+        openMenuBtn.setAttribute('aria-expanded', false);
+        bodyScrollLock.enableBodyScroll(document.body);
+    });
+    
+})();
