ef0e7e7dddab0b2c0c14c28e0d6c5029ace66aed
Iryna R <deutsche.post15@gmail.com>
2024-08-19 16:34:55 +0200

    add border for input

--- src/js/work-together.js
+++ src/js/work-together.js
@@ -20,33 +20,35 @@ const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 const successMessage = document.querySelector(".success");
 const errorMessage = document.querySelector(".error");
 const emailInput = document.querySelector("input#email");
-
-const currentEmail = document.querySelector(".current.work-together-label-email");
-const currentComments = document.querySelector(".current.work-together-label-comments");
+const commentsInput = document.querySelector("input#comments");
 
 populateForm();
+checkFormInput();
 
 form.addEventListener("input", handleFormInput);
 form.addEventListener("submit", handleClickBtnSend);
 
+function checkFormInput(){
+    if (emailPattern.test(emailInput.value)) {
+        successMessage.style.display = 'block';
+        emailInput.style.borderBottomColor = '#3CBC81';
+        errorMessage.style.display = 'none';
+    } else {
+        errorMessage.style.display = 'block';
+        emailInput.style.borderBottomColor = '#E74A3B';
+        successMessage.style.display = 'none';
+    }
+    if(commentsInput.value){
+        commentsInput.style.borderBottomColor = 'rgba(250, 250, 250, 0.50)';
+    }
+}
+
 function handleFormInput(){
-    successMessage.style.display = 'none';
-    errorMessage.style.display = 'none';
 
     formData.email = form.elements.email.value.trim();
     formData.comment = form.elements.comments.value.trim();
 
-    if (emailPattern.test(emailInput.value)) {
-        successMessage.style.display = 'block';
-        currentEmail.style.setProperty('--after-color', '#3CBC81');
-    } else {
-        errorMessage.style.display = 'block';
-        currentEmail.style.setProperty('--after-color', '#E74A3B');
-    }
-
-    if(formData.comment){
-        currentComments.style.setProperty('--after-color', 'rgba(250, 250, 250, 0.50)');
-    }
+    checkFormInput();
 
     localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
@@ -55,10 +57,11 @@ async function handleClickBtnSend(event){
     event.preventDefault();
     successMessage.style.display = 'none';
     errorMessage.style.display = 'none';
-    document.documentElement.style.setProperty('--after-color', 'rgba(250, 250, 250, 0.20))');
+    emailInput.style.borderBottomColor = 'rgba(250, 250, 250, 0.20)';
+    commentsInput.style.borderBottomColor = 'rgba(250, 250, 250, 0.20)';
 
     if(!formData.email || !formData.comment){
-        iziToast.show({
+        return iziToast.show({
             title: 'Caution',
             titleColor: '#FAFAFB',
             titleSize: '16px',
@@ -70,14 +73,33 @@ async function handleClickBtnSend(event){
             messageSize: '16px',
             messageLineHeight: ' 24px',
             backgroundColor: '#FFA000',
+            close: 'true',
         });
     }
 
     try{
         await addData(formData);
         form.reset();
+        localStorage.removeItem(STORAGE_KEY);
+        return iziToast.show({
+            title: 'Thank you for your interest in cooperation!',
+            titleColor: '#FAFAFA',
+            titleSize: '20px',
+            titleLineHeight: '30px',
+            message: `The manager will contact you shortly to discuss further details and opportunities for cooperation. Please stay in touch.`,
+            messageColor: 'rgba(250, 250, 250, 0.60)',
+            messageSize: '16px',
+            messageLineHeight: ' 24px',
+            backgroundColor: '#1C1D20',
+            position: 'center',
+            maxWidth: '492px',
+            progressBar: 'false',
+            closeOnEscape: 'true',
+            close: 'true',
+        });
+
     }catch(err){
-        iziToast.show({
+        return iziToast.show({
             title: 'Error',
             titleColor: '#FAFAFB',
             titleSize: '16px',
@@ -88,14 +110,11 @@ async function handleClickBtnSend(event){
             messageColor: '#FAFAFB',
             messageSize: '16px',
             messageLineHeight: ' 24px',
-            backgroundColor: '#EF4040',
+            backgroundColor: '#ED3B44',
+            close: 'true',
         });
     }
-
-    localStorage.removeItem(STORAGE_KEY);
 }
-
-
 
 function populateForm(){
     let savedFormData = {}
@@ -114,7 +133,8 @@ function populateForm(){
             messageColor: '#FAFAFB',
             messageSize: '16px',
             messageLineHeight: ' 24px',
-            backgroundColor: '#EF4040',
+            backgroundColor: '#ED3B44',
+            close: 'true',
         });
         return;
     }
