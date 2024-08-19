95b8a82b81c504fcce095ea6a545e558e7f9ce1c
Tetiana Posmashna <tetiana.posmashna@gmail.com>
2024-08-19 18:27:14 +0300

    Update faq.js try-catch

--- src/js/faq.js
+++ src/js/faq.js
@@ -2,18 +2,21 @@
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 
+try {
+    const accordionFaq = new Accordion('.accordion-container', {
+        duration: 400,
+        showMultiple: true,
+    });
 
-const accordionFaq = new Accordion('.accordion-container', {
-    duration: 400,
-    showMultiple: true,
-});
+    const faqBtns = document.querySelectorAll('.faq-item-btn');
 
-const faqBtns = document.querySelectorAll('.faq-item-btn');
-
-faqBtns.forEach(button => {
-    button.addEventListener('click', () => {
-        const arrowIcon = button.querySelector('.faq-btn-icon');
-        arrowIcon.classList.toggle('arrow-up');
-    })
-});
-
+    faqBtns.forEach(button => {
+        button.addEventListener('click', () => {
+            const arrowIcon = button.querySelector('.faq-btn-icon');
+            arrowIcon.classList.toggle('arrow-up');
+        })
+    });
+}
+catch (error){
+console.error ("Error", error)
+}
